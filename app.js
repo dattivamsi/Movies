633e53e5620022b344fbc6c0fddc5ebff1e9bfff
dattivamsi <dattitarun@gmail.com>
2023-05-09 10:28:53 +0000

    commit message

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const dataBasePath = path.join(__dirname, "moviesData.db");
+
+const app = express();
+
+app.use(express.json());
+
+let db = null;
+
+const initializeDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dataBasePath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => console.log("http://loaalhost:3000"));
+  } catch (e) {
+    console.log(`DbError:${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
+
+const convertMovieDbToResponseObject = (dbObject) => {
+  movieId: dbObject.movie_id;
+  directorId: dbObject.director_id;
+  movieName: dbObject.movie_name;
+  leadActor: dbObject.lead_actor;
+};
+
+const directorDbToResponseObject = (dbObject) => {
+  directorId: dbObject.director_id;
+  director_name: dbObject.director_name;
+};
+
+app.get("/movies/", async (request, response) => {
+  const getMoviesQuery = `
+    select * from movie;
+    `;
+  const allMovies = await db.all(getMoviesQuery);
+  response.send(
+    allMovies.map((eachMovie) => ({
+      movieName: eachMovie.movie_name,
+    }))
+  );
+});
+
+app.post("/movies/", async (request, response) => {
+  const { directorId, movieName, leadActor } = request.body;
+  const postMovie = `
+  INSERT into  movie(director_id,movie_name,lead_actor) values ('${directorId}','${movieName}','${leadActor}');`;
+  const movie = await db.run(postMovie);
+  response.send("Movie Successfully Added");
+});
+
+app.get("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const idBasedMovie = `
+  select * from movie
+  WHERE movie_id = ${movieId};`;
+  const movieK = await db.get(idBasedMovie);
+  response.send(convertMovieDbToResponseObject(movieK));
+});
+
+module.exports = app;
